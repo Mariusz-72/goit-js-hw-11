65bb3c466b1fe1951d56d2c0f686729e60d4f0e1
Mariusz-72 <mmbwdap@gmail.com>
2023-10-05 23:12:24 +0200

    update 1.0

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-
-import SlimSelect from 'slim-select';                            //import narzędzi
-import 'slim-select/dist/slimselect.css';
-import Notiflix from 'notiflix';
-import 'notiflix/dist/notiflix-3.2.6.min.css';
-
