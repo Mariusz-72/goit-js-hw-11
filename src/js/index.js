f23bc36011abe723a2884a5356dc7b4c1e884c67
Mariusz-72 <mmbwdap@gmail.com>
2023-10-05 23:55:38 +0200

    update 1.1
    index.js początek

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,10 @@ import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const buttonLoadMore = document.querySelector('.load-more');
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+let page = 1;
