c7191fbe2c9b416e0ca04506881364ba61dec9fe
Mariusz-72 <mmbwdap@gmail.com>
2023-10-06 17:22:29 +0200

    update 2.0

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const apiKey = '39862890-65ac2b6f59b7905db114a6f69';
+
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const buttonLoadMore = document.querySelector('.load-more');
@@ -12,3 +14,66 @@ const buttonLoadMore = document.querySelector('.load-more');
 const lightbox = new SimpleLightbox('.gallery a');
 
 let page = 1;
+
+searchForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    gallery.innerHTML = '';
+    page = 1;
+
+    const searchQuery = event.target.elements.searchQuery.value;
+
+    await fetchImages(searchQuery, page);
+})
+
+async function fetchImages(query, page) {
+    
+    const perPage = 40;
+}
+
+const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+ 
+try {
+    const response = await axios.get(url);
+    const { data } = response;
+
+    if (data.totalHits === 0) {
+        Notiflix.Notify.failure("Sorry, there are no images matching ypur search query. Please try again.");
+        buttonLoadMore.style.display = 'none';
+        return;
+    }
+
+    const images = data.hits.map((image) => `
+        <a a href = "${image.largeImageURL}" class = "photo-card" target = "_blanc" >
+    <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        <div class="info">
+            <p class="info-item">
+            <b>Likes</b>: ${image.likes}
+            </p>
+            <p class="info-item">
+            <b>Views</b>: ${image.views}
+            </p>
+            <p class="info-item">
+            <b>Comments</b>: ${image.comments}
+            </p>
+            <p class="info-item">
+            <b>Downloads</b>: ${image.downloads}
+            </p>
+        </div>
+        </a>
+        `);
+    
+    gallery.insertAdjacentHTML('beforeend', images.join(''));
+    lightbox.refresh();
+    
+    if (data.totalHits <= page * perPage) {
+        buttonLoadMore.style.display = 'none';
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+    } else {
+        buttonLoadMore.style.display = 'block';
+    }
+} catch (error) {
+    Notiflix.Notify.failure("Oops! Something went wrong. Please try again.");
+}
+
+    
+    
