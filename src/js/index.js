a65832f452af25abf9a8a4d1509bce94457c414f
Mariusz-72 <mmbwdap@gmail.com>
2023-10-07 23:35:14 +0200

    update 3.5

--- src/js/index.js
+++ src/js/index.js
@@ -29,20 +29,19 @@ searchForm.addEventListener('submit', async (event) => {
 async function fetchImages(query, page) {
     const perPage = 40;
     const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
- 
+
     try {
         const response = await axios.get(url);
         const { data } = response;
-
         if (data.totalHits === 0) {
             Notiflix.Notify.failure("Sorry, there are no images matching ypur search query. Please try again.");
             buttonLoadMore.style.display = 'none';
             return;
         }
 
-        const images = data.hits.map((image) => `
+const images = data.hits.map((image) => `
         <a href = "${image.largeImageURL}" class="photo-card" target="_blank" >
-    <img class="photo-card__image" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        <img class="photo-card__image" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
         <div class="info">
             <p class="info-item"><b>Likes</b> ${image.likes}
             </p>
@@ -54,7 +53,7 @@ async function fetchImages(query, page) {
             </p>
         </div>
         </a>
-        `);
+    `);
     
         gallery.insertAdjacentHTML('beforeend', images.join(''));
         lightbox.refresh();
@@ -69,9 +68,9 @@ async function fetchImages(query, page) {
         Notiflix.Notify.failure("Oops! Something went wrong. Please try again.");
     }
 }
- 
+
 buttonLoadMore.addEventListener('click', () => {
-  page++;
-  fetchImages(searchQuestion, page);
+    page++;
+    fetchImages(searchQuestion, page);
 });
 
