e86b5f559c42381e72242589950b688e0760c4cb
Mariusz-72 <mmbwdap@gmail.com>
2023-10-07 21:50:22 +0200

    update 3.0

--- src/js/index.js
+++ src/js/index.js
@@ -14,66 +14,64 @@ const buttonLoadMore = document.querySelector('.load-more');
 const lightbox = new SimpleLightbox('.gallery a');
 
 let page = 1;
+let searchQuestion = '';
+
+buttonLoadMore.style.display = 'none';
 
 searchForm.addEventListener('submit', async (event) => {
     event.preventDefault();
     gallery.innerHTML = '';
     page = 1;
-
-    const searchQuery = event.target.elements.searchQuery.value;
-
-    await fetchImages(searchQuery, page);
-})
+    searchQuery = event.target.elements.searchQuery.value;
+    fetchImages(searchQuery, page);
+});
 
 async function fetchImages(query, page) {
-    
-    const perPage = 40;
-}
-
-const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+    const perPage = 10;
+    const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
  
-try {
-    const response = await axios.get(url);
-    const { data } = response;
+    try {
+        const response = await axios.get(url);
+        const { data } = response;
 
-    if (data.totalHits === 0) {
-        Notiflix.Notify.failure("Sorry, there are no images matching ypur search query. Please try again.");
-        buttonLoadMore.style.display = 'none';
-        return;
-    }
+        if (data.totalHits === 0) {
+            Notiflix.Notify.failure("Sorry, there are no images matching ypur search query. Please try again.");
+            buttonLoadMore.style.display = 'none';
+            return;
+        }
 
-    const images = data.hits.map((image) => `
-        <a a href = "${image.largeImageURL}" class = "photo-card" target = "_blanc" >
+        const images = data.hits.map((image) => `
+        <a href = "${image.largeImageURL}" class="photo-card" target="_blanc" >
     <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
         <div class="info">
-            <p class="info-item">
-            <b>Likes</b>: ${image.likes}
+            <p class="info-item"><b>Likes</b>: ${image.likes}
             </p>
-            <p class="info-item">
-            <b>Views</b>: ${image.views}
+            <p class="info-item"><b>Views</b>: ${image.views}
             </p>
-            <p class="info-item">
-            <b>Comments</b>: ${image.comments}
+            <p class="info-item"><b>Comments</b>: ${image.comments}
             </p>
-            <p class="info-item">
-            <b>Downloads</b>: ${image.downloads}
+            <p class="info-item"><b>Downloads</b>: ${image.downloads}
             </p>
         </div>
         </a>
         `);
     
-    gallery.insertAdjacentHTML('beforeend', images.join(''));
-    lightbox.refresh();
+        gallery.insertAdjacentHTML('beforeend', images.join(''));
+        lightbox.refresh();
     
-    if (data.totalHits <= page * perPage) {
-        buttonLoadMore.style.display = 'none';
-        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-    } else {
-        buttonLoadMore.style.display = 'block';
+        if (data.totalHits <= page * perPage) {
+            buttonLoadMore.style.display = 'none';
+            Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+        } else {
+            buttonLoadMore.style.display = 'block';
+        }
+    } catch (error) {
+        Notiflix.Notify.failure("Oops! Something went wrong. Please try again.");
     }
-} catch (error) {
-    Notiflix.Notify.failure("Oops! Something went wrong. Please try again.");
 }
+ 
+buttonLoadMore.addEventListener('click', () => {
+  page++;
+  fetchImages(searchQuestion, page);
+});
 
-    
-    
